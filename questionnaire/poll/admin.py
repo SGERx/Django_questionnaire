b125e84ba7a6f1499d6077b9a463421848077d24
OutEastCheshire <winampagent007@mail.ru>
2024-02-05 20:57:01 +0300

    Basic Django Admin panel added, needs redacting

--- questionnaire/poll/admin.py
+++ questionnaire/poll/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Answer, AnswerOption, Participant, Poll, Question
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'participants', 'created_on', 'redacted')
+    search_fields = ('title',)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'poll', 'participants', 'answered_rating', 'question_text', 'created_on', 'redacted')
+    search_fields = ('question_text',)
+
+
+class AnswerOptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'option_text', 'created_on', 'redacted')
+    search_fields = ('option_text',)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'participant', 'question', 'response_text', 'response_date')
+    search_fields = ('response_text',)
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'created_on', 'last_login')
+    search_fields = ('username', 'email')
+
+
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(AnswerOption, AnswerOptionAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Question, QuestionAdmin)
--- questionnaire/poll/models.py
+++ questionnaire/poll/models.py
@@ -1,1 +1,61 @@
 from django.db import models
+
+
+class Participant(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password_hash = models.CharField(max_length=255)
+    password_salt = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'participants'
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    participants = models.IntegerField(default=0)
+    created_on = models.DateTimeField()
+    redacted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'polls'
+
+
+class Question(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    participants = models.IntegerField(default=0)
+    answered_rating = models.IntegerField(default=0)
+    question_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    redacted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'questions'
+
+
+class AnswerOption(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    redacted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'answer_options'
+
+
+class Answer(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    response_text = models.TextField(null=True, blank=True)
+    response_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = False
+        db_table = 'answers'
